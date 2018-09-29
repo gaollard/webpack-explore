6511fe1fd433775a9b3485ae504786abe0aae515
gaollard <gaollard@foxmail.com>
2018-09-29 23:29:12 +0800

    first commit

---
+++ 01-webpack.config.js
@@ -0,0 +1,13 @@
+const path = require('path')
+const project = '01-entry-array'
+function resolve (dir) {
+  return path.resolve(__dirname, dir)
+}
+const config = {
+  mode: "development",
+  entry: [
+    resolve(project + '/foo.js'),
+    resolve(project + '/bar.js')
+  ]
+};
+module.exports = config;
